6f516998a055d17675208cafb8a37f58ca567968
Kate Makarenka <polumna-la@mail.ru>
2018-02-24 18:04:02 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,29 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count=0
+  let arr=[""].concat(preferences)
+  for (i=0; i<arr.length; i++) {
+    if  (isNaN(arr[i])) { continue 
+    } else {
+      let first=arr[i];
+      let second=arr[first];
+      let third=arr[second];
+    
+        if(first && second && third && first != second && second != third && first != third && third === i){
+            arr[i] = arr[first] = arr[second] = '';
+            count++
+        }
+      
+    }
+    
+  }
+    return count;
+  
+
+   
+  
+  
+
+  
 };
